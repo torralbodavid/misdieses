0eb4c2b34f4c2eb5828b65dca35e37fdc2068824
David Torralbo <davidtorralboperez@gmail.com>
2021-10-04 22:17:59 +0200

    Add first image response

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,19 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param Request $request
-     *
-     * @return void
-     */
     public function __invoke(Request $request)
     {
-        return abort(503, 'A ver espérese');
+        return response()->file('storage/memes/test.webp', ['image/webp']);
     }
 }
