6296fa139b7559319dab274ff360ef2cd99dcea5
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2021-10-11 14:11:19 +0300

    Fake files in memes directory to prevent tests from failing when contents of memes directory is not png

--- tests/Feature/HomepageTest.php
+++ tests/Feature/HomepageTest.php
@@ -2,22 +2,28 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class HomepageTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function it_returns_a_random_meme()
     {
+        Storage::fake('local');
+
+        Storage::disk('local')->putFile(HomeController::MEME_DIRECTORY, UploadedFile::fake()->image('image-1.png'));
+        Storage::disk('local')->putFile(HomeController::MEME_DIRECTORY, UploadedFile::fake()->image('image-2.png'));
+        Storage::disk('local')->putFile(HomeController::MEME_DIRECTORY, UploadedFile::fake()->image('image-2.png'));
 
         $this->get('/')
             ->assertStatus(200)
-            ->assertHeader('Cache-control','must-revalidate, no-cache, no-store, public')
-            ->assertHeader('Content-Type','image/png');
+            ->assertHeader('Cache-control', 'must-revalidate, no-cache, no-store, public')
+            ->assertHeader('Content-Type', 'image/png');
     }
 }
